ee85a9a77d2160dbfec900995cc2ebb6633a8216
Tin <2002trungtin@gmail.com>
2025-07-01 14:36:57 +0700

    adding example images

--- p2 + 3/my-app/src/App.js
+++ p2 + 3/my-app/src/App.js
@@ -3,8 +3,8 @@ import App2 from "./App2";
 import App3 from "./App3";
 
 function App() {
-  // return <App2 />;
-  return <App3 />;
+  return <App2 />;
+  // return <App3 />;
 }
 
 export default App;
